cd33706c9d05e3e4237fdbb0dc2d8e0d00335911
kyle.l.frisbie@gmail.com <kyle.l.frisbie@gmail.com>
2015-09-02 17:38:29 -0600

    added code for api call to blog site

--- app/src/main/java/com/kylefrisbie/blogviewer/app/ListActivity.java
+++ app/src/main/java/com/kylefrisbie/blogviewer/app/ListActivity.java
@@ -1,18 +1,70 @@
 package com.kylefrisbie.blogviewer.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 
 public class ListActivity extends ActionBarActivity {
+
+
+
+    protected static ArrayList<String> getContenxtWeb() {
+        String pagina = "", devuelve = "";
+        JSONArray jsonArray;
+        ArrayList<JSONObject> joAry= new ArrayList<JSONObject>();
+        ArrayList<String> titleAry = new ArrayList<String>();
+
+
+        URL url;
+        try {
+            url = new URL("http://www.kylefrisbie.com/api/blogposts");
+            HttpURLConnection conexion = (HttpURLConnection) url
+                    .openConnection();
+            conexion.setRequestProperty("User-Agent",
+                    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+            if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(conexion.getInputStream()));
+                String linea = reader.readLine();
+                while (linea != null) {
+                    pagina += linea;
+                    linea = reader.readLine();
+                }
+                reader.close();
+
+                devuelve = pagina;
+            } else {
+                conexion.disconnect();
+                return null;
+            }
+            conexion.disconnect();
+
+            jsonArray  = new JSONArray(devuelve); // json
+            for (int i = 0; i < jsonArray.length(); i++) {
+                titleAry.add(i, jsonArray.getJSONObject(i).getString("title"));
+            }
+
+            for (int i = 0; i < titleAry.size(); i++) {
+                System.out.println(titleAry.get(i));
+            }
+
+            return titleAry;
+        } catch (Exception ex) {
+            return titleAry;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +75,15 @@ public class ListActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        Log.w("TAG", "entering getContenxtWeb()");
+
+//        ListView myListView = (ListView)findViewById(R.id.listView);
+//        adapter = new CustomAdapter(getActivity(), R.layout.row, myStringArray1);
+//        myListView.setAdapter(new );
+
+        getContenxtWeb();
+
     }
 
 
